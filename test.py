720a48cb45529261b8d7c3a69ec60b4fb3e029db
Eric-BM <57955362+Eric-BM@users.noreply.github.com>
2022-06-01 15:28:00 +0000

    Wed, Jun 1, 2022, 3:28 PM +00:00

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
 import os
 pop_folder_names = []
-for roots, dirs, files in os.walk(
-        r"C:/Users/1243712/OneDrive - Jabil/Desktop/Work/POP 2022"):
-    for dir in dirs:
-        print(dir)
+for roots, dirs, files in os.walk(r"C:/Users/1243712/OneDrive - Jabil/Desktop/Work/POP 2022"):
+    print("1")
 """        if dir.rfind('%s' % pop_period) != -1:
             pop_folder_names.append(dir[14:18])"""
 
